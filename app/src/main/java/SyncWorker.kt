df2d2e48e2001088ff910ef8c11bd558a0a15bbd
danaj zigic <Danaj.Zigic@saltsav.com>
2022-09-24 16:50:00 -0400

    Create syncworker class

---
+++ app/src/main/java/SyncWorker.kt
@@ -0,0 +1,15 @@
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class SyncWorker(appContext: Context, workerParams: WorkerParameters):
+    Worker(appContext, workerParams) {
+    override fun doWork(): Result {
+
+
+
+        return Result.success()
+    }
+
+
+}
